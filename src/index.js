5a5cc8f6422c52de6d33cd53edb241a805b71aaf
feh-franc0 <fernandofv1110@gmail.com>
2022-09-06 02:46:28 -0300

    API de tarefas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 const http = require('http')
 
+const wait = (time) =>
+  new Promise(resolve =>
+    setTimeout(resolve, time)
+  )
+
+const todosDatabase = (() => {
+  let idSequence = 1
+  const todos = {}
+
+  const insert = async (todo) => {
+    await wait(500)
+    const id = idSequence++
+    const data = { ...todo, id }
+    todos[id] = data
+    return data
+  }
+
+  const list = async () => {
+    await wait(100)
+    return Object.values(todos)
+  }
+
+  const get = async (id) => {
+    await wait(100)
+    return todos[id]
+  }
+
+  const update = async (todo) => {
+    await wait(500)
+    todos[todo.id] = todo
+    return todo
+  }
+
+  const del = async (id) => {
+    await wait(500)
+    delete todos[id]
+  }
+
+  return {
+    insert,
+    list,
+    get,
+    update,
+    del,
+  }
+
+})()
+
+const JsonHeader = { 'Content-Type': 'application/json' }
+
 const server = http.createServer((request, response) => {
 
   // GET /hello/:nome -> Hello ${name}
@@ -30,8 +80,36 @@ const server = http.createServer((request, response) => {
 
   // {id, title, text}
 
-  // POST /todos
+  // POST /todos { "text": "string", "title": "string" }
+  if (request.method === 'POST' && request.url.startsWith('/todos')) {
+    let bodyRaw = ''
+
+    request.on('data', data => bodyRaw += data)
+
+    request.once('end', () => {
+      const todo = JSON.parse(bodyRaw)
+      todosDatabase.insert(todo)
+        .then(inserted => {
+          response.writeHead(201, JsonHeader)
+          response.end(JSON.stringify(inserted))
+        })
+    })
+
+    return
+  } //* curl -X POST http://localhost:3000/todos -d '{"title": "dar a aula", "text": "não esquecer os endpoints de todos" }'
+
+
   // GET /todos
+  if (request.method === 'GET' && request.url.startsWith('/todos')){
+    todosDatabase
+      .list()
+      .then(todos => {
+        response.writeHead(200, JsonHeader)
+        response.end(JSON.stringify({ todos }))
+      })
+    return
+  } //* curl -X GET http://localhost:3000/todos
+
   // GET /todos/:id
   // DELETE /todos/:id
   // PUT /todos/:id
